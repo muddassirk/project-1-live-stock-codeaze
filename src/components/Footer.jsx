048efe89b409c03168fde0ec46d2269242a2d377
muddassirk <mkkhanu72@gmail.com>
2021-09-20 14:00:11 +0500

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='text-center'>
+            <p className='mx-auto my-0 p-0'> 2021 Working Sign in, All right Reserved | Easy Qurbani</p>
+        </div>
+    )
+}
+
+export default Footer

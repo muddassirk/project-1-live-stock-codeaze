a30bbe045d740051c4a3f27bb3f65998d0072b52
muddassirk <mkkhanu72@gmail.com>
2021-09-21 18:14:39 +0500

    create BuyerDashboard

--- src/components/BuyerDashboard/BuyerDashboard.jsx
+++ src/components/BuyerDashboard/BuyerDashboard.jsx
@@ -4,6 +4,7 @@ import NavBar from '../Constant/NavBar'
 import ForSaleCard from '../UI/Buyer/ForSaleCard'
 import SideBarCard from '../UI/Buyer/SideBarCard'
 import SubCard from '../UI/Buyer/SubCard'
+import Carousel from '../Constant/Carousel'
 
 const BuyerDashboard = () => {
     return (
@@ -15,33 +16,7 @@ const BuyerDashboard = () => {
                     Welcome to Website
                 </h1>
             </div> */}
-            <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={0} className="active" aria-current="true" aria-label="Slide 1" />
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={1} aria-label="Slide 2" />
-                    {/* <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={2} aria-label="Slide 3" /> */}
-                </div>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src="https://timelinecovers.pro/facebook-cover/download/stunning-little-flowers-facebook-cover.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://cdn.pixabay.com/photo/2018/07/17/14/43/banner-3544296__480.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                    {/* <div className="carousel-item active">
-                        <img src="https://timelinecovers.pro/facebook-cover/download/stunning-little-flowers-facebook-cover.jpg" alt="..." />
-                    </div> */}
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true" />
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true" />
-                    <span className="visually-hidden">Next</span>
-                </button>
-            </div>
-
+                <Carousel />
             <div className='container'>
                 <div className='row'>
                     <SubCard />

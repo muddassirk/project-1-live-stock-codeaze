584af7e13346654174ced35249ae39e2bc9beae7
muddassirk <mkkhanu72@gmail.com>
2021-09-21 18:16:42 +0500

    create VenderDetail

---
+++ src/components/Vender/VenderDetail.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+const VenderDetail = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="card p-4">
+                    <div className='py-4'>
+                        <h2>
+                            Registerd Vendors
+                        </h2>
+                    </div>
+                    <div className="card-body text-center">
+                        <div className="row border border-dark py-2">
+                            <div className="col-sm-6">
+                                <h4>
+                                    Vendors
+                                </h4>
+                            </div>
+                            <div className="col-sm-6">
+                                <h4>
+                                    Actions
+                                </h4>
+                            </div>
+                        </div>
+                        <div className="row border border-dark py-2">
+                            <div className="col-sm-6">
+                                <h4>
+                                    Vendors
+                                </h4>
+                            </div>
+                            <div className="col-sm-6">
+                                <h4>
+                                    Actions
+                                </h4>
+                            </div>
+                        </div>
+                        <div className="row border border-dark py-2">
+                            <div className="col-sm-6">
+                                <h4>
+                                    Vendors
+                                </h4>
+                            </div>
+                            <div className="col-sm-6">
+                                <h4>
+                                    Actions
+                                </h4>
+                            </div>
+                        </div>
+                        <div className="row border border-dark py-2">
+                            <div className="col-sm-6">
+                                <h4>
+                                    Vendors
+                                </h4>
+                            </div>
+                            <div className="col-sm-6">
+                                <h4>
+                                    Actions
+                                </h4>
+                            </div>
+                        </div>
+                        <div className="row border border-dark py-2">
+                            <div className="col-sm-6">
+                                <h4>
+                                    Vendors
+                                </h4>
+                            </div>
+                            <div className="col-sm-6">
+                                <h4>
+                                    Actions
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default VenderDetail

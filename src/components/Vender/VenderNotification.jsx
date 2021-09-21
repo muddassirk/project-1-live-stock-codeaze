52a4f874e6c13f609e6a86a2fd364d7c09e5da17
muddassirk <mkkhanu72@gmail.com>
2021-09-21 18:16:56 +0500

    create VenderNotification

---
+++ src/components/Vender/VenderNotification.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const VenderNotification = () => {
+    return (
+        <div>
+            <h1>
+                Notification
+            </h1>
+        </div>
+    )
+}
+
+export default VenderNotification

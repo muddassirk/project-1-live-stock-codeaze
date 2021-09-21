4749ef770e4bd673de60795d35c46208f9a1d44d
muddassirk <mkkhanu72@gmail.com>
2021-09-21 18:16:25 +0500

    create VenderDashboard

---
+++ src/components/Vender/VenderDashboard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const VenderDashboard = () => {
+    return (
+        <div>
+            <div className="card">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <h2>$232</h2>
+                            <small>Sales</small>
+                        </div>
+                        <div className="col-md-4">
+                            <h2>$232</h2>
+                            <small>Earnings</small>
+
+                        </div>
+                        <div className="col-md-4">
+                            <h2>$0</h2>
+                            <small>Page View</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default VenderDashboard

0f2c9bebd6d1ab51c349eef2f92b5c2b89b9b9b6
muddassirk <mkkhanu72@gmail.com>
2021-09-21 12:01:18 +0500

    subCard for Buyer Dash

---
+++ src/components/UI/Buyer/SubCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SubCard = () => {
+    return (
+        <div className="card my-5" style={{ width: '17rem' }}>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    )
+}
+
+export default SubCard
